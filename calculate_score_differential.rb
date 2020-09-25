db5f6c40b46f053df3952110322ad25a555eba02
Dennis Marchand <dennis.marchand@arcade.co>
2020-09-25 10:27:37 -0700

    Add PCC adjustment

--- calculate_score_differential.rb
+++ calculate_score_differential.rb
@@ -2,9 +2,9 @@ class CalculateScoreDifferential
   STANDARD_SLOPE_RATING = 113.freeze
   DECIMAL_POINTS_TO_ROUND_TO = 1.freeze
 
-  def call(adjusted_gross_score, course_rating, slope_rating)
+  def call(adjusted_gross_score, course_rating, slope_rating, pcc_adjustment = 0)
     raw_differential =
-      (adjusted_gross_score - course_rating) *
+      (adjusted_gross_score - course_rating - pcc_adjustment) *
       (STANDARD_SLOPE_RATING.fdiv(slope_rating))
 
     raw_differential.round(DECIMAL_POINTS_TO_ROUND_TO)
